f0f8813135ff390b298e0ccb27280315b0884b00
DerjavaGIT <noooman26@gmail.com>
2022-10-01 15:32:22 +0300

    firstprogramm

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿// Напишите программу ,которая на вход принимает число и выдает его квадрат
+// Натуральные числа unsigned int
+// Целые числа int
+// Дробный числа double (float)
+
 int a = 0;
-string userEnter = Console.ReadLine();
+string userEnter = Console.ReadLine()!;
 
 int.Parse(userEnter);
 
 int b = a * a;
+b = (int)Math.Pow(a, 2);
