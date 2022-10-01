47ab8fd14ae5edf8c1c4db8d6f619467eb13e69d
DerjavaGIT <noooman26@gmail.com>
2022-10-01 15:50:21 +0300

    firstprogramm

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@
 // Дробный числа double (float)
 
 int a = 0;
+Console.Write($"Введите число: ");
 string userEnter = Console.ReadLine()!;
 
 int.Parse(userEnter);
 
 int b = a * a;
+
 b = (int)Math.Pow(a, 2);
+
+Console.Write($"{a} ^ 2 = {b}");
